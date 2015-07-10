1629e0c761b8449c56a413249e4948764f0be0fd
Alex Miller <alex.k.miller@gmail.com>
2015-07-10 16:13:59 +0100

    Working base.html. Next step: images

--- ka/blog/views.py
+++ ka/blog/views.py
@@ -5,12 +5,17 @@ from blog.models import Post
 from blog.models import Tag
 from blog.models import Stock
 from blog.models import About
+from blog.utils import *
 
 def index(request):
-    #get the blog posts that are published
+    #Filter by tags and queries
+    queryParam = request.GET.get('q')
     tagParam = request.GET.get('tag')
     if tagParam:
         post_list = Post.objects.filter(published=True, tag__slug=tagParam)
+    elif queryParam:
+        postQuery = get_query(queryParam, ['title', 'content','description','tag__name','stock__symbol','stock__company'])
+        post_list = Post.objects.filter(postQuery,published=True).distinct()
     else:
         post_list = Post.objects.filter(published=True)
     paginator = Paginator(post_list,4) # Show 4 posts per page
@@ -32,15 +37,23 @@ def index(request):
             post.stocks = Stock.objects.filter(post__slug=post.slug)
             stockLen += len(post.stocks)
     #now return the rendered template
-    return render(request,'blog/index.html',{'posts':posts,'tags':tags,'half_tags':half_tags,'tagParam':tagParam,'stockLen':stockLen})
+    return render(request,'blog/index.html',{'posts':posts,'tags':tags,'half_tags':half_tags,'tagParam':tagParam,'queryParam':queryParam,'stockLen':stockLen})
 
 def about(request):
     about = About.objects.all()[:1].get()
-    return render(request,'blog/about.html',{'about':about})
+    #Tags
+    tags = Tag.objects.all()
+    half_tags = math.ceil(len(tags)/2.0)
+    return render(request,'blog/about.html',{'about':about,'tags':tags,'half_tags':half_tags})
 
 def post(request,slug):
     #get the Post object
     post = get_object_or_404(Post,slug=slug)
-    post.tags = Tag.objects.filter(post__slug=post.slug)
+    #Tags
+    tags = Tag.objects.filter(post__slug=post.slug)
+    half_tags = math.ceil(len(tags)/2.0)
+    #Stocks
+    post.stocks = Stock.objects.filter(post__slug=post.slug)
+    stockLen = len(post.stocks)
     #now return the rendered template
-    return render(request,'blog/post.html',{'post':post})
+    return render(request,'blog/post.html',{'post':post,'tags':tags,'half_tags':half_tags,'stockLen':stockLen})
